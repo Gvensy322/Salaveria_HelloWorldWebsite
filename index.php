31ef1bfc2785a17edf5e6a629d53706b9942373f
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-31 10:55:39 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    
+    <title>Learn Heroku - Hello World!</title>
+  </head>
+  <body>
+    <h>Helo world!!! </h>
+<style>
+h1 {
+  color: teal;
+  border: 3px teal solid;
+  font-size: 6em;
+  padding: 1em;
+  margin: 0.5em;
+  text-align: center;
+  font-family: sans-serif;
+}
+</style>
+  </body>
