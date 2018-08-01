e2552a76cd8084aa7aae2d191605eb962b66aea0
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-02 06:06:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,7 @@
   <body>
     <h>Helo world!!! </h>
 <style>
-h1 {
-  color: teal;
-  border: 3px teal solid;
-  font-size: 6em;
-  padding: 1em;
-  margin: 0.5em;
-  text-align: center;
-  font-family: sans-serif;
-  }
+
 
   
 </style>
